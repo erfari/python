9b067afc1704a60e234e75f51483ac48a75d10ac
erfari <erfari@mail.ru>
2018-04-09 15:08:12 +0300

    Add files via upload

---
+++ import_csv_for.py
@@ -0,0 +1,47 @@
+# Импортируем необходимые библиотеки
+import pandas as pd
+import sqlalchemy
+import mysql.connector
+import csv
+
+# Создание соединения с нашей базой данных
+config = {
+    'user': 'root',
+    'password': '',
+    'host': '127.0.0.1',
+    'database': 'bookmate2'
+}
+conn = mysql.connector.connect(**config)
+
+#Делаем запросы и получаем результаты
+cursor = conn.cursor()
+
+#Создаем таблицу в бд с помощью execute
+#Таблица с двумя столбцами user_id и started_at 
+cursor.execute("CREATE TABLE USERS ( \
+                user_id int (10) AUTO_INCREMENT, \
+                started_at TIMESTAMP NOT NULL, \
+                PRIMARY KEY (user_id)); ")
+
+#Импортируем таблицы в бд
+
+with open('C:/Users/23/analyst_test.csv') as csvfile:
+    reader = csv.DictReader(csvfile, delimiter = ',')
+    for row in reader:
+        print(row['user_id'], row['started_at'])
+#Соединение с бд
+        config = {
+    'user': 'root',
+    'password': '',
+    'host': '127.0.0.1',
+    'database': 'bookmate2'
+                }
+        conn = mysql.connector.connect(**config)
+        sql_statement = "INSERT INTO USERS(user_id , started_at) VALUES (%s,%s)"
+        cur = conn.cursor()
+        cur.executemany(sql_statement,[(row['user_id'], row['started_at'])])
+        conn.commit()
+        
+
+# Не забываем закрыть соединение с базой данных
+conn.close()
---
+++ import_csv_sqlalchemy.py
@@ -0,0 +1,15 @@
+#Импортируем необходимые модули
+import pandas as pd
+import sqlalchemy
+#Читаем csv файл
+frame = pd.read_csv('C:/Users/23/analyst_test.csv')
+#Создаем соединение с базой данных
+database_username = 'root'
+database_password = ''
+database_ip       = 'localhost'
+database_name     = 'bookmate'
+database_connection = sqlalchemy.create_engine('mysql+mysqlconnector://{0}:{1}@{2}/{3}'.
+                                               format(database_username, database_password, 
+                                                      database_ip, database_name))
+#Импортируем данные в бд. to_sql создаст таблицу, а также столбцы, если они отсутствуют в бд.
+frame.to_sql(con=database_connection, name='users', if_exists='replace')
